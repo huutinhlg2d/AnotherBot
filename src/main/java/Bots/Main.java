3cde657db06c35fe0ae1725cd31d0be6b9f29e72
minkylg2d <74044387+minkylg2d@users.noreply.github.com>
2021-05-28 22:19:56 +0700

    Add files via upload

---
+++ src/main/java/Bots/Main.java
@@ -0,0 +1,23 @@
+package Bots;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+
+public class Main {
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA TutorialBot = JDABuilder.createDefault(Secret.TutorialBotToken,
+            GatewayIntent.GUILD_MESSAGES,
+            GatewayIntent.DIRECT_MESSAGES,
+            GatewayIntent.GUILD_VOICE_STATES, 
+            GatewayIntent.GUILD_EMOJIS,
+            GatewayIntent.GUILD_MESSAGE_REACTIONS)
+            .addEventListeners(new Listener())
+            .setActivity(Activity.playing("Use chovk"))
+                .build().awaitReady();
+    }
+}
---
+++ src/main/java/Bots/Secret.java
@@ -0,0 +1,6 @@
+package Bots;
+
+public class Secret {
+    public static String TutorialBotToken = System.getenv("TOKEN");
+}
+
